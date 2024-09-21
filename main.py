df08407b4f8cde4c26e160102da926ba99f9d9b1
alans <alanshenjc@gmail.com>
2024-09-21 23:08:39 +0300

    final mock sourcodes if want email function to work change the send email function and import smtp and change time.sleep as desired

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import requests
 # selector lib extract the information we need
 import selectorlib
 
+import time
+
 URL = 'https://programmer100.pythonanywhere.com/tours/'
 
 def scrape(url):
@@ -11,6 +13,29 @@ def scrape(url):
     source = response.text
     return source
 
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+def send_email():
+    print('Email was sent')
+
+def store(extracted):
+    with open('data.txt','a') as file:
+        file.write(extracted +'\n')
+def read(extracted):
+    with open('data.txt','r')as file:
+       return file.read()
 
 if __name__ == "__main__":
-    print(scrape(URL))
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+        content = read(extracted)
+        if extracted!="No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_email(messages="Hey, a new event was found!")
+        time.sleep(2)
