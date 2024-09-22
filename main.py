20d73f216847ccb69dfb5bf7373527db41190d63
alans <alanshenjc@gmail.com>
2024-09-22 17:33:17 +0300

    utilizing the sql instead of data.txt

--- main.py
+++ main.py
@@ -2,8 +2,14 @@
 import requests
 # selector lib extract the information we need
 import selectorlib
-
 import time
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+
+
+"INSERT INTO events VALUES('Tigers', 'Tiger City', '2088.10.14')"
+
 
 URL = 'https://programmer100.pythonanywhere.com/tours/'
 
@@ -22,20 +28,30 @@ def send_email():
     print('Email was sent')
 
 def store(extracted):
-    with open('data.txt','a') as file:
-        file.write(extracted +'\n')
+    row = extracted.split(',')
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES (?,?,?)", row)
+    connection.commit()
+
 def read(extracted):
-    with open('data.txt','r')as file:
-       return file.read()
+    row = extracted.split(',')
+    row = [item.strip() for item in row]
+    band,city,date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band = ? AND city = ? AND date =?", (band,city,date))
+    rows = cursor.fetchall()
+    return rows
 
 if __name__ == "__main__":
     while True:
         scraped = scrape(URL)
         extracted = extract(scraped)
         print(extracted)
-        content = read(extracted)
+
         if extracted!="No upcoming tours":
-            if extracted not in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
-                send_email(messages="Hey, a new event was found!")
+                send_email()
         time.sleep(2)
